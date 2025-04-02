dd5e22a0df94adbdf647a9fc98a0ddd4f5ef61b8
FaribaNA <faribanaghizadehavilagh@gmail.com>
2025-04-02 11:00:39 +0200

    Add 01. Add main scrip. Add tidy processed data.

---
+++ R/01_tidy_data.R
@@ -0,0 +1,31 @@
+# Tidy up hydroclimate variables 
+
+## Load source files
+
+source("source/main.R")
+
+unzip(paste0(RAW_PATH,"aggregated_daily.zip"), exdir = RAW_PATH)
+
+csv_files <- list.files(RAW_PATH, full.names = TRUE, pattern = ".csv")
+
+## Reading all at once and create a single dt
+
+all_data <- rbindlist(lapply(csv_files, function(file) {
+  dt <- fread(file)
+  dt[, SITE_ID := tools::file_path_sans_ext(basename(file))]  
+  return(dt)
+}))
+
+# Melting data
+tidy_data <- melt(
+  all_data,
+  id.vars = c("TIMESTAMP", "SITE_ID"),  
+  variable.name = "variable",           
+  value.name = "value"                   
+)
+
+setnames(tidy_data, old= c("TIMESTAMP", "SITE_ID", "variable", "value"),
+         new= c("timestamp", "site_id", "variable", "value"))
+
+# save data 
+saveRDS(tidy_data, paste0(PROCESSED_PATH,"tidy_data.rds"))
---
+++ source/main.R
@@ -0,0 +1,11 @@
+## Required libraries
+
+library(data.table)
+library(tools)
+library(dplyr)
+library(raster)
+
+## paths
+RAW_PATH <- "data/raw/"
+PROCESSED_PATH <- "data/processed/"
+
